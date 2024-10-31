0df0faf5d601b0ab24f8728bf3f5fe99d96c79bf
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-10-31 18:46:57 -0300

    added 'create' method

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,7 +1,11 @@
 class PreferencesController < ApplicationController
   def index
-    binding.pry
     @preferences = current_user.preferences
     @pagy, @records = pagy(@preferences)
+  end
+
+  def create
+    preference_params = params.require(:preference).permit(:name, :description, :restriction)
+    @preference = current_user.preferences.build(preference_params)
   end
 end
