50343e7ad7b7bfa02a53fbffd0b399aec1f380e9
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-04 11:52:32 -0300

    Updated name field to disallow null entries

--- db/migrate/20241021175734_create_preferences.rb
+++ db/migrate/20241021175734_create_preferences.rb
@@ -2,7 +2,7 @@ class CreatePreferences < ActiveRecord::Migration[7.1]
   def change
     create_table :preferences do |t|
       t.references :user, foreign_key: true, null: false
-      t.string :name
+      t.string :name, null: false
       t.text :description
       t.boolean :restriction, null: false, default: true
 
