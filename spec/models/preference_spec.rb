43f4dd9f9181522d9f40dc3276866318196a84a2
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-05 13:03:43 -0300

    Added tests to check validations

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -17,5 +17,19 @@
 require 'rails_helper'
 
 RSpec.describe Preference, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    Preference.new(
+      name: "test",
+      description: "test",
+      restriction: false
+    )
+  }
+  it 'is valid with a name' do
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
