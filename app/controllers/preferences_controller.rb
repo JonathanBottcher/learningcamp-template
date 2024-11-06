4315dcda93007c4d96d7181b3bdcaf08fab33122
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-06 11:10:04 -0300

    Added remaining preferences actions

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,15 +1,52 @@
 class PreferencesController < ApplicationController
+  before_action :set_preference, only: %i[edit update destroy show]
+
   def index
     @preferences = current_user.preferences
     @pagy, @records = pagy(@preferences)
   end
-  
+
+  def show
+  end
+
   def new
     @preference = Preference.new
   end
 
+  def edit
+  end
+
   def create
-    preference_params = params.require(:preference).permit(:name, :description, :restriction)
     @preference = current_user.preferences.build(preference_params)
+
+    if @preference.save
+      redirect_to preferences_path, notice: 'Preference was created successfully'
+    else
+      render :new, alert: 'Could not create preference'
+    end
+  end
+
+  def update
+    if @preference.update(preference_params)
+      redirect_to @preference, notice: 'Preference was updated successfully'
+    else
+      render :edit, alert: 'Could not update preference'
+    end
+  end
+
+  def destroy
+    if @preference.destroy
+      redirect_to preferences_path, notice: 'Preference was succesfully deleted'
+    else
+      redirect_to preferences_path, alert: 'Could not delete preference'
+    end
+  end
+
+  def preference_params
+    params.require(:preference).permit(:name, :description, :restriction)
+  end
+
+  def set_preference
+    @preference = current_user.preferences.find(params[:id])
   end
 end
