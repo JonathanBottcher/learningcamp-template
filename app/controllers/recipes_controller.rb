d311b3d7bdce6e1e8e75b6ce3867169f15458857
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-07 10:50:47 -0300

    added create and new actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,20 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def show; end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+  end
+
+  def destroy; end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :ingredients)
+  end
 end
