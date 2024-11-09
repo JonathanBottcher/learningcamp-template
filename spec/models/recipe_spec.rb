8552e40f30d0edd28b89763e89c9468a561e0925
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-09 11:36:32 -0300

    Added basic tests for the recipe model

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id          :bigint           not null, primary key
+#  user_id     :bigint           not null
+#  name        :string           not null
+#  description :text             not null
+#  ingredients :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_recipes_on_user_id  (user_id)
+#
+
+describe Recipe do
+  describe 'validations' do
+    subject { build(:recipe) }
+
+    let(:user) { create(:user) }
+
+    it 'is valid with a name, description and ingredients' do
+      expect(subject).to be_valid
+    end
+  end
+end
