67eb5060ebff5031abcfaed3eeb970a7b9d2a0f2
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-09 11:37:29 -0300

    Updated factories

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id          :bigint           not null, primary key
+#  user_id     :bigint           not null
+#  name        :string           not null
+#  description :text             not null
+#  ingredients :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_recipes_on_user_id  (user_id)
+#
+FactoryBot.define do
+  factory :recipe do
+    name { 'MyString' }
+    description { 'MyText' }
+    ingredients { 'MyText' }
+    user
+  end
+end
