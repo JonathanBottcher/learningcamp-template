7982cabfaa2c428c6702c6cc12984f6aa99212df
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-09 11:34:24 -0300

    Implemented recipe generator service into the controller and added show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,33 @@
 # frozen_string_literal: true
 
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
   def index
     @recipes = Recipe.all
   end
 
-  def show; end
+  def show
+    @recipe
+  end
 
   def new
     @recipe = Recipe.new
   end
 
   def create
-    @recipe = current_user.recipes.build(recipe_params)
+    @recipe = RecipeGeneratorService.new(recipe_params[:ingredients], current_user.id).call
+
+    if @recipe.save
+      redirect_to recipes_path, notice: t('views.recipes.create_success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 
   def recipe_params
