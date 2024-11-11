ad8cda07287368c04911daa9562c92d2bb19a6e9
Jonathan Bottcher <51808808+JonathanBottcher@users.noreply.github.com>
2024-11-11 12:44:37 -0300

    Update preference_spec.rb
    Added validations and test for max preferences.

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -24,5 +24,27 @@ describe Preference do
     it 'is valid with a name, description and restriction' do
       expect(subject).to be_valid
     end
+    
+    # Validate presence
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:restriction) }
+
+    # Test for max preferences
+    it 'is invalid if number of preferences is exceeded' do
+      user.preferences.create!(name: 'Preference 1', description: 'Description 1',
+                               restriction: true)
+      user.preferences.create!(name: 'Preference 2', description: 'Description 2',
+                               restriction: true)
+      user.preferences.create!(name: 'Preference 3', description: 'Description 3',
+                               restriction: true)
+      user.preferences.create!(name: 'Preference 3', description: 'Description 3',
+                               restriction: true)
+      user.preferences.create!(name: 'Preference 4', description: 'Description 4',
+                               restriction: true)
+
+      extra_preference = user.preferences.build(name: 'Preference 6', description: 'Description 6', restriction: true)
+      expect(extra_preference).not_to be_valid
+    end
   end
 end
